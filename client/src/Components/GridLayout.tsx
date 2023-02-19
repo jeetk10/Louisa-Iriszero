0397840e864283296a834fb96edaa5411010781b
Vinol Fernandes <Vinolfernandes20@gmail.com>
2023-02-19 14:51:33 +0530

    cards

--- client/src/Components/GridLayout.tsx
+++ client/src/Components/GridLayout.tsx
@@ -4,35 +4,64 @@ import { Card, CardHeader, CardBody, CardFooter,Flex,Avatar,Heading,Text,IconBut
 import data from '../../../flask-server/data/tweets.json';
 import Cards from '../Components/Card'
 
-const GridLayout = () => {
- 
 
-  return (
-    
-    data && data.map( records=>{
+const GridLayout = () => {
+  return(
+    <Box alignItems="center">
+      {data.map(each_title=>{
       return(
-        records && records.map(innerData=>{
-          return(
-          innerData && innerData.map(x=>{
+        <>
+        <Heading>{each_title.title_of_article}</Heading>
+          {each_title.tweets.map(tweet=>{
             return(
-              <Box>
-              <Heading colorScheme={"whiteAlpha"} >{x.title_of_article}</Heading>
-              <SimpleGrid spacing={4}>
-              {x.tweets && x.tweets.map(content=>{
-                return(
-                <><Cards userName="Team Iris_Zero" tweetContent={content.tweet_content} /><div id="root"></div></>
-                )
-              })
-            }
-              </SimpleGrid >
-            </Box>
+              <SimpleGrid>
+                <Cards 
+                  userName={tweet.user_info.name}
+                  tweetContent={tweet.tweet_text} 
+                  profileImage={tweet.user_info.profile_image_url} 
+                  screenName={tweet.user_info.screen_name}
+                  mediaURL={tweet.tweet_url}
+                  // video={tweet.tweet_url}
+                />
+            </SimpleGrid>
             )
-        })
-          )
-        })
+          }
+          )}
+        </> 
       )
     })
-  );
+  }
+    </Box>
+    
+    
+  )
+
+  // return (
+    
+  //   data && data.map( records=>{
+  //     return(
+  //       records && records.map(innerData=>{
+  //         return(
+  //         innerData && innerData.map(x=>{
+  //           return(
+  //             <Box>
+  //             <Heading colorScheme={"whiteAlpha"} >{x.title_of_article}</Heading>
+  //             <SimpleGrid spacing={4}>
+  //             {x.tweets && x.tweets.map(content=>{
+  //               return(
+  //               <><Cards userName="Team Iris_Zero" tweetContent={content.tweet_content} /><div id="root"></div></>
+  //               )
+  //             })
+  //           }
+  //             </SimpleGrid >
+  //           </Box>
+  //           )
+  //       })
+  //         )
+  //       })
+  //     )
+  //   })
+  // );
 };
 
 export default GridLayout;
