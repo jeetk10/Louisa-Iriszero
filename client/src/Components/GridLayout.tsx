729481c7677463ac139bacfb860af9ef456dbb3d
a-bhattacharya <abhattacharya@protonmail.com>
2023-02-20 17:21:35 +0530

    Minor UI improvements

--- client/src/Components/GridLayout.tsx
+++ client/src/Components/GridLayout.tsx
@@ -1,63 +1,115 @@
-import { Button, Center, Grid, GridItem, SimpleGrid, Spacer } from "@chakra-ui/react";
+import {
+  Button,
+  Center,
+  Grid,
+  Link,
+  GridItem,
+  SimpleGrid,
+  Spacer,
+  LinkBox,
+} from "@chakra-ui/react";
 import { Box, Container } from "@chakra-ui/react";
-import { Card, CardHeader, CardBody, CardFooter,Flex,Avatar,Heading,Text,IconButton,Image} from '@chakra-ui/react'
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Flex,
+  Avatar,
+  Heading,
+  Text,
+  IconButton,
+  Image,
+} from "@chakra-ui/react";
 import { GrLinkPrevious, GrLinkNext } from "react-icons/gr";
-import data from '../../../flask-server/data/tweets.json';
-import Cards from '../Components/Card'
+import data from "../../../flask-server/data/tweets.json";
+import Cards from "../Components/Card";
 
-
-const image=data.map(each_title=>{
-  return(
-    each_title.tweets.map( video=>{
-      return(video.tweet_images_url)
-    })
-  )
-})
+const image = data.map((each_title) => {
+  return each_title.tweets.map((video) => {
+    return video.tweet_images_url;
+  });
+});
 console.log(image);
 const GridLayout = () => {
-  return(
+  return (
     <div>
-      {data.map(each_title=>{
-      return(
-        each_title.tweets && each_title.tweets.length>0 ?    //Does not return the news articles which contain 0 tweets
-        <>
-        <Flex marginTop={10}>
-          <Button variant='solid' colorScheme={"teal"} leftIcon={<GrLinkPrevious/>}>Previous</Button>
-          <Spacer/>
-          <Heading><Center><a>{each_title.title_of_article}</a></Center></Heading>
-          <Spacer/>
-          {/* onClick={nextPage()} */}
-           <Button variant='solid' colorScheme={"teal"}   leftIcon={<GrLinkNext/>}>Next</Button>
-        </Flex>
-        <SimpleGrid columns={4}>
-          {each_title.tweets.map(tweet=>{
-            return(
-              
-                <Cards 
-                  userName={tweet.user_info.name}
-                  tweetContent={tweet.tweet_text} 
-                  profileImage={tweet.user_info.profile_image_url} 
-                  screenName={tweet.user_info.screen_name}
-                  mediaURL={tweet.tweet_images_url}
-                  likeCount={tweet.tweet_likecount}
-                  retweet={tweet.tweet_retweetcount}
-                  video={tweet.tweet_video_info?.variants?.map(video_urls=>{
-                    return(video_urls.url)
-                  })}
-                />
-            
-            )
-          }
-          )}
-          </SimpleGrid>
-          
-        </> 
-        :null
-      )
-    })
-  }
+      {data.map((each_title) => {
+        return each_title.tweets && each_title.tweets.length > 0 ? ( //Does not return the news articles which contain 0 tweets
+          <>
+            <Flex marginTop={10}>
+              <Button
+                variant="outline"
+                mx="2em"
+                colorScheme="blue"
+                leftIcon={<GrLinkPrevious />}
+              >
+                Previous
+              </Button>
+              <Spacer />
+              <Heading
+                boxShadow="lg"
+                bgGradient="linear(to-t,gray.100,gray.200)"
+              >
+                <Center
+                  textAlign="justify"
+                  px="0.5em"
+                  pb="0.2em"
+                  pt="0.15em"
+                  fontFamily="serif"
+                >
+                  <Link
+                    sx={{
+                      textDecoration: "none",
+                      color: "gray.800",
+                      transition: "box-shadow 0.3s",
+                      "&:hover": {
+                        textDecor: "none",
+                        color: "gray.600",
+                        boxShadow: "0 1px 2px rgba(0, 0, 0, 0.25)",
+                      },
+                    }}
+                  >
+                    {each_title.title_of_article}
+                  </Link>
+                </Center>
+              </Heading>
+              <Spacer />
+              {/* onClick={nextPage()} */}
+              <Button
+                variant="outline"
+                mx="2em"
+                colorScheme="blue"
+                leftIcon={<GrLinkNext />}
+              >
+                Next
+              </Button>
+            </Flex>
+            <SimpleGrid columns={4}>
+              {each_title.tweets.map((tweet) => {
+                return (
+                  <Cards
+                    userName={tweet.user_info.name}
+                    tweetContent={tweet.tweet_text}
+                    profileImage={tweet.user_info.profile_image_url}
+                    screenName={tweet.user_info.screen_name}
+                    mediaURL={tweet.tweet_images_url}
+                    likeCount={tweet.tweet_likecount}
+                    retweet={tweet.tweet_retweetcount}
+                    video={tweet.tweet_video_info?.variants?.map(
+                      (video_urls) => {
+                        return video_urls.url;
+                      }
+                    )}
+                  />
+                );
+              })}
+            </SimpleGrid>
+          </>
+        ) : null;
+      })}
     </div>
-  )
+  );
 
   //  async function nextPage(){
   //   return(window.location.replace(""))
--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -1,17 +1,21 @@
-import { Center, Container, Heading } from "@chakra-ui/react";
+import { Center, Container, Text, Heading } from "@chakra-ui/react";
 
 function Header() {
   return (
     <Center
       bgGradient="linear(to-l, blue.500, blue.200)"
       // pos="absolute"
+      display="block"
       w="100%"
+      h="5.5em"
       p="2"
       boxShadow="lg"
       color="white"
       textAlign="center"
     >
       <Heading fontFamily="serif">TWEET GENERATOR</Heading>
+
+      <Text fontFamily="Courier">IrisZero</Text>
     </Center>
   );
 }
